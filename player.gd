78bfa2a04e4b3323031c79c8d5ef4ebf755a4a95
VentCat <73477803+dsxksss@users.noreply.github.com>
2023-03-10 22:59:47 +0800

    update

--- player.gd
+++ player.gd
@@ -1,19 +1,19 @@
 extends CharacterBody2D
 
-
+# 速度值
 const SPEED = 100.0
+# 跳跃值
 const JUMP_VELOCITY = -200.0
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
+# 从项目设置里获取重力值
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-
 func _physics_process(delta):
-	# Add the gravity.
+	# 添加角色重力
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
-	# Handle Jump.
+	# 处理跳跃
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
